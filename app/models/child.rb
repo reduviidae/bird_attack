e98e6352e8ad6bf0379c07e4bffe614a8f3c97d3
kenneth-yu <kyu22@buffalo.edu>
2019-01-24 12:12:55 -0500

    Validation Bug Fix

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,7 +2,7 @@ class Child < ApplicationRecord
   has_many :attacks
   has_many :birds, through: :attacks
   validates :name, length:{ minimum: 3, maximum: 15}
-  validates :password, length:{ minimum: 5, maximum: 15}
-  validates :name, :password, presence: true
+  validates :password, length:{ minimum: 5, maximum: 15}, on: :create
+  validates :name, :password, presence: true, on: :create
   has_secure_password
 end
